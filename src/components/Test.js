6c5814a9c54cfb5e23cb3ea5ede4259ea1882062
onnar <50223443+onnar@users.noreply.github.com>
2019-06-05 18:28:56 +0900

    Create React Project

---
+++ src/components/Test.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Test extends Component {
+    render(){
+        return(
+            <h2>Hi Test.js</h2>
+        );
+    }
+}
+
+export default Test;
